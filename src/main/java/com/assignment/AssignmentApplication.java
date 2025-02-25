1b5110577a5d03aa7904c0aaca6ab056f64dc3fd
balajirakh <rakhbalaji77@gmail.com>
2025-02-25 12:14:27 +0530

    new changes

--- src/main/java/com/assignment/AssignmentApplication.java
+++ src/main/java/com/assignment/AssignmentApplication.java
@@ -9,6 +9,7 @@ public class AssignmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentApplication.class, args);
 		System.out.println("hii balaji");
+		System.out.println("hii rs");
 	}
 
 }
